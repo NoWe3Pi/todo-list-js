9565f9689887ae9de7b59a93cde2d0ebe6098217
Dmitriy <nowe3pi@yandex.ru>
2026-01-22 23:27:37 +0300

    initial task_2

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-
+//***********************1.создания и управления списком задач
 
 const tasks = [];
 
 function addTask(title, description) {
   // Используем современный способ генерации ID или хотя бы текущее время const id = Date.now() + Math.floor(Math.random() * 100)
   //но тут использую метод .randomUUID() объекта crypto, который генерирует уникальный идентификатор
-  const id = crypto.randomUUID()
+  const id = crypto.randomUUID();
 
   const newTask = {
     id,
-    title,       // Сокращенная запись (shorthand property), если ключ и значение совпадают
+    title, // Сокращенная запись (shorthand property), если ключ и значение совпадают
     description,
-    status: 'todo',
-    createdAt: new Date()
+    status: "todo",
+    createdAt: new Date(),
   };
 
   tasks.push(newTask);
@@ -22,12 +22,66 @@ function addTask(title, description) {
 addTask("Изучить объекты", "Пройти материалы 4-го блока");
 console.table(tasks); // Специальный метод для красивого вывода массивов с объектами
 
+////***********************2.Фильтрация массива объектов по числовому условию
 
+const smartphones = [
+  { name: "Samsung Galaxy A54", price: 28000 }, // item, значение 0
+  { name: "iPhone 15 Pro", price: 110000 },
+  { name: "Xiaomi Redmi Note 12", price: 18500 },
+  { name: "Google Pixel 8", price: 65000 },
+  { name: "Nothing Phone (2)", price: 45000 },
+  { name: "Realme C53", price: 12000 },
+];
+// Сравнение стоимости двух смартфонов по их индексам в массиве
+if (smartphones[0].price > smartphones[1].price) {
+  console.log(`Этот смартфон дороже и круче: ${smartphones[0].name}`);
+} else {
+  console.log(`Вне конкуренции: ${smartphones[1].name}`);
+}
 
+const expensivePhones = smartphones.filter(function (item) {
+  return item.price > 15000;
+});
 
+console.table(expensivePhones);
 
+// Обработка массива: добавление динамических скидок и категорий.
+let phonesWithDiscount = expensivePhones.map((item) => {
+  const newItem = {};
 
+  newItem.name = item.name;
+  newItem.price = item.price;
 
+  /*  *Закоментировал условия, так как решил оставить тернарный оператор для этой задачи
+    if(newItem.discountPrice > 80000){
+     newItem.category = "Premium";
+   }else if(newItem.discountPrice > 40000) {
+         newItem.category = "Middle";
+   } else {newItem.category = "Budget";}
+ */
 
+  // Логика динамической скидки
+  let discountPercent;
+  if (item.name.includes("iPhone")) {
+    discountPercent = 0.05;
+  } else if (item.name.includes("Samsung")) {
+    discountPercent = 0.15;
+  } else {
+    discountPercent = 0.1;
+  }
 
+  newItem.discount = newItem.price * discountPercent;
+  newItem.discountPrice = newItem.price - newItem.discount;
 
+  // Логика категоризации
+  newItem.category =
+    newItem.discountPrice > 80000
+      ? "Premium"
+      : newItem.discountPrice > 40000
+      ? "Middle"
+      : "Budget";
+
+  return newItem;
+});
+
+console.log(phonesWithDiscount);
